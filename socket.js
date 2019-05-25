7c6b3e996255aad29c1bf81c0c97138f35600f83
anhaemin <kebinkastora@gmail.com>
2019-05-25 22:31:37 +0900

    서버의 socket에 웹 소켓 이벤트 연결

--- socket.js
+++ socket.js
@@ -1,31 +1,29 @@
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
-  const io = SocketIO(server, { path: '/socket.io' });
-
-  io.on('connection', (socket) => {
-    const req = socket.request;
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
-    
+module.exports = (server, app) => {
+  const io = SocketIO(server, { path: '/socket.io'});
+  app.set('io', io);
+  const room = io.of('/room');
+  const chat = io.of('/chat');
+  room.on('connection', (socket) => {
+    console.log('room 네임스페이스에 접속');
     socket.on('disconnect', () => {
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
+      console.log('room 네임스페에스 접속 해제');
     });
-
-    socket.on('error', (error) => {
-      console.error(error);
+  });
+  
+  chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스에 접속');
+    const req = socket.request;
+    const { headers: { referer } } = req;
+    const roomId = referer
+      .split('/')[referer.split('/').length -1]
+      .replace(/\?.+/, '');
+    socket.join(roomId);
+    socket.on('disconnect', () => {
+      console.log('chat 네임스페이스 접속 해제');
+      socket.leave(roomId);
     });
-
-    socket.on('reply', (data) => {
-      console.log('data: ', data);
-    })
-
-    socket.interval = setInterval(() => {
-      socket.emit("news", "Hello Socket.IO! ");
-    }, 3000);
-    
   });
 };
-
 
